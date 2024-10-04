cfe400aba162c9dcf30463abbda88eaeb26e4c5f
Philip Ashton <philligan1@gmail.com>
2024-10-03 20:19:06 -0500

    through main, starting stretch

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,9 +2,29 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+const maxItems = 5;
 
 
+function addItem (item) {
+    console.log( 'in addItem');
+    basket.push( item);
+    return true;
 
+}
+
+function listItems () {
+    console.log('in listItems');
+    for( let i=0; i<basket.length; i++) {
+        console.log (basket [i]);
+    }
+    return true
+}
+
+function empty () {
+    console.log('in empty');
+    basket.length = 0;
+}
 
 
 
